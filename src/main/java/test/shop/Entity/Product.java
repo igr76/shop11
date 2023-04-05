e32579a5b46ac6fa7f928dd3be1d5ae27a5a5dc7
igr76 <gritsuk76@mail.ru>
2023-04-05 22:02:12 +1000

    entity

--- src/main/java/test/shop/Entity/Product.java
+++ src/main/java/test/shop/Entity/Product.java
@@ -1,7 +1,6 @@
 package test.shop.Entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import java.util.ArrayList;
